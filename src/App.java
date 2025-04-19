fdb66c39e1ac43dc9a60fdd9499206f786e6171c
mohamed aboubakr <mabobakr365@gmail.com>
2025-04-19 19:40:26 +0200

    Added some enums features

--- src/App.java
+++ src/App.java
@@ -5,13 +5,38 @@ public class App{
     float appVersion;
 
     public enum Operation {
-        ADDITION,
-        MULTIPLICATION,
-        SUBTRACTION,
-        DIVISION
+        ADDITION {
+            // Overriding the supertype method tells Java "Hey, now I'm using my own version from this provided method."
+            @Override
+            public int applyOperation(int num1, int num2) {
+                return num1 + num2;
+            }
+        },
+        MULTIPLICATION {
+            // Using the Override annotation to help the compiler.
+            @Override
+            public int applyOperation(int num1, int num2) {
+                return num1 * num2;
+            }
+        },
+        SUBTRACTION {
+            @Override
+            public int applyOperation(int num1, int num2) {
+                return num1 - num2;
+            }
+        },
+        DIVISION {
+            @Override
+            public int applyOperation(int num1, int num2) {
+                return num1 / num2;
+            }
+        };
+        /* Providing the method and make it (abstract), to tell Java that each constant will define its own version of
+        this method, here we're just saying "Hey, here is the method, and you define it with your own" */
+        public abstract int applyOperation(int num1, int num2);
     }
 
-    public App (String name, float version) {
+    App (String name, float version) {
         if (version < 0) {
             throw  new IllegalArgumentException("Version can't be negative though!");
         }
@@ -35,23 +60,9 @@ public class App{
     public int operating(int num1, int num2, String operationType) {
         int result = 0;
         try {
-            Operation choosenOperation = Operation.valueOf(operationType.toUpperCase());
-            switch (choosenOperation) {
-                case ADDITION:
-                    result = num1 + num2;
-                    break;
-                case MULTIPLICATION:
-                    result = num1 * num2;
-                    break;
-                case SUBTRACTION:
-                    result = num1 - num2;
-                    break;
-                case DIVISION:
-                    result = num1 / num2;
-                    break;
-                default:
-                    break;
-            }
+            // Choosing the correspondence Enum of the user input
+            Operation chosenOperation = Operation.valueOf(operationType.toUpperCase());
+            result =  chosenOperation.applyOperation(num1, num2);
         } catch (IllegalArgumentException exception) {
             System.out.println("Invalid operation type: " + operationType);
         }
