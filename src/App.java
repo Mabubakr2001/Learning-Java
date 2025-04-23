1b17cb58fba7f88f3e224b088ce80d13b3ee3e06
mohamed aboubakr <mabobakr365@gmail.com>
2025-04-23 21:05:22 +0200

    Added some comments

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 package src;
 
+// we don't have the abstract keyword because everything inside the interface is assumed to be abstract
 public interface App {
     void greeting(String name);
     void countdown();
